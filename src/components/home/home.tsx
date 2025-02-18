29d082cae2927dc83861fdf988447012818b84f3
Alexis Harrell <alexisharrell@Alexiss-MacBook-Pro.local>
2025-02-17 21:35:42 -0500

    init layout

---
+++ src/components/home/home.tsx
@@ -0,0 +1,9 @@
+const Home = () => {
+  return (
+    <div className='container mx-auto my-5 px-5 flex justify-center'>
+      <div>klwdnlakwdlkajnwldkna</div>
+    </div>
+  )
+}
+
+export default Home
---
+++ src/components/navbar/navbar.tsx
@@ -0,0 +1,11 @@
+const NavBar = () => {
+  return (
+    <div className='navbar bg-base-100 h-30 pr-10 pl-10'>
+      <div className='navbar-start'>1</div>
+      <div className='navbar-center'>2</div>
+      <div className='navbar-end'>3</div>
+    </div>
+  )
+}
+
+export default NavBar
